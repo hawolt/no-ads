e70855dc9d26abb502c11bdc05176f51da92a331
hawolt <45050975+hawolt@users.noreply.github.com>
2026-01-19 15:39:16 +0100

    update rust wrapper, java tray icon and extension logs (#11)
    * update wrapper to work with workflow jdk
    
    * update tray icon from generic name to no-ads
    
    * use no-ads name as extension init print

--- rust-wrapper/src/main.rs
+++ rust-wrapper/src/main.rs
@@ -1,5 +1,6 @@
 #![windows_subsystem = "windows"]
 
+use std::fs;
 use std::fs::File;
 use std::io::{self, Cursor, Write};
 use std::os::windows::process::CommandExt;
@@ -16,6 +17,7 @@ fn main() -> io::Result<()> {
     let mut archive = ZipArchive::new(cursor)?;
     for i in 0..archive.len() {
         let mut file = archive.by_index(i)?;
+        println!("{}", file.name()); // prints the full path inside the zip
         let out_path = dir.path().join(file.mangled_name());
         if file.is_dir() {
             let dir_path = Path::new(&out_path);
@@ -35,7 +37,14 @@ fn main() -> io::Result<()> {
     let path = dir.path().join("application.jar");
     let mut file = File::create(&path)?;
     file.write_all(include_bytes!("../application.jar"))?;
-    let java = dir.path().join("jre").join("bin").join("java.exe");
+    let java = fs::read_dir(dir.path())?
+        .filter_map(|entry| {
+            let entry = entry.ok()?;
+            let java_path = entry.path().join("bin").join("java.exe");
+            if java_path.exists() { Some(java_path) } else { None }
+        })
+        .next()
+        .expect("Could not find javaw.exe in extracted JRE");
     Command::new(java)
         .stdout(Stdio::null())
         .stderr(Stdio::null())
